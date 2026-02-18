129f9037e2663025aec023e52ba4cdf65c4d0899
iTrue <109562378+carlosyamanaka@users.noreply.github.com>
2026-02-18 13:42:53 -0300

    Initial commit

---
+++ src/main/java/io/github/carlosyamanaka/cyphvv/CyphvvApplication.java
@@ -0,0 +1,13 @@
+package io.github.carlosyamanaka.cyphvv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyphvvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyphvvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/carlosyamanaka/cyphvv/CyphvvApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.carlosyamanaka.cyphvv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyphvvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
