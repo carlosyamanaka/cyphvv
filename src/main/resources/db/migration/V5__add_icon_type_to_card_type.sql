c546a6e0945f42f84049a4cf8aee99bb02aa89bc
iTrue <109562378+carlosyamanaka@users.noreply.github.com>
2026-05-25 14:56:50 -0300

    added if not exists in sql script flyway

--- src/main/resources/db/migration/V5__add_icon_type_to_card_type.sql
+++ src/main/resources/db/migration/V5__add_icon_type_to_card_type.sql
@@ -1,2 +1,2 @@
--- Adiciona a coluna icon_type na tabela card_type
-ALTER TABLE card_type ADD COLUMN icon_type VARCHAR(255);
+-- Adiciona a coluna icon_type na tabela card_type se ela ainda não existir
+ALTER TABLE card_type ADD COLUMN IF NOT EXISTS icon_type VARCHAR(255);
